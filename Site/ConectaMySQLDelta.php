b2b60cfbb90ab81f8c2b746acaac8954818028e8
Wende(ll)veloper <110984453+Wen-developer@users.noreply.github.com>
2023-05-04 21:34:18 -0300

    conexão banco de dados atualizada

--- Site/ConectaMySQLDelta.php
+++ Site/ConectaMySQLDelta.php
@@ -1,17 +1,14 @@
 <?php
-    function conectSQL(){
-        //Dados para conexao ao MySQL → MySQL
-$host = '144.22.157.228';        
-$port = 3306;
-$user = 'delta';
-$senha = 'delta';
-$dbname = 'Delta';
- try{
-    //Mostra a String de Conexao ao MySQL → Criei a String de conexão e conectei ao banco (PDO)
-return new PDO("mysql:host=$host:$port;dbname=$dbname", $user, $senha);
-    } catch (PDOException $e) {
-        echo "Erro: " . $e->getMessage();
-        throw $e;
-    }
+    function conectaSQL(){
+       //Dados para conexao ao MySQL → MySQL
+$mysqlhostname = "144.22.157.228";
+$mysqlport = "3306";
+$mysqlpassword="delta";
+$mysqlusername = "delta";
+$mysqldatabase = "Delta";
+
+//Mostra a String de Conexao ao MySQL → Criei a String de conexão e conectei ao banco (PDO)
+$dsn = 'mysql:host=' . $mysqlhostname . ';dbname=' . $mysqldatabase . ';port=' . $mysqlport;
+$pdo = new PDO($dsn, $mysqlusername, $mysqlpassword);
+return $pdo;
 }
-?>
