db3a9d94f9c672b4db0153690475819581b6877d
Wende(ll)veloper <110984453+Wen-developer@users.noreply.github.com>
2023-05-05 21:19:31 -0300

    Query insert ajustada

--- Site/TelaGerenciarProdutos/InsertProdutos.php
+++ Site/TelaGerenciarProdutos/InsertProdutos.php
@@ -1,19 +1,20 @@
-<?php
- require("ConectaMySQLDelta");
- if(isset($_GET['enviar'])){
-    if (!empty($_GET['nomeProduto'])|| !empty($_GET['descProduto']) || !empty($_GET['categoriaProd']) || !empty($_GET['qntProduto']) || !empty($_GET['precoProduto']) || !empty($_GET['descontoProduto'] || !empty($_GET['categoriaID']))
-     $nome = $_GET['nomeProduto'];
-     $preco = $_GET['precoProduto'];
-     $desc = $_GET['descProduto'];
-     $categoria = $_GET['categoriaProd'];
-     $quantidade = $_GET['qntProduto'];
-     $desconto = $_GET['descontoProduto'];
-     $categoriaID = $_GET['categoriaID'];
-
-        $comandosql = "INSERT INTO PRODUTO(PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, CATEGORIA_ID) VALUES ('$nome', '$desc', '$preco', '$desconto', '$categoriaID')";
-        $comandosqlQuantidade = "INSERT INTO PRODUTO_ESTOQUE(PRODUTO_QTD) VALUES('$quantidade')";
-        $enviar = 
-
-
+<?php include "ConectaMySQLDelta.php";
+	$pdo = conectSQL();
+ if (isset($_POST['enviar'])) {
+    if (!empty($_POST['nomeProduto'])|| !empty($_POST['descProduto']) || !empty($_POST['categoriaProd']) || !empty($_POST['qntProduto']) || !empty($_POST['precoProduto']) || !empty($_POST['descontoProduto']) || !empty($_POST['categoriaID'])) {
+      $nome = $_POST['nomeProduto'];
+      $preco = $_POST['precoProduto'];
+      $desc = $_POST['descProduto'];
+      $categoria = $_POST['categoriaProd'];
+      $quantidade = $_POST['qntProduto'];
+      $desconto = $_POST['descontoProduto'];
+      $categoriaID = $_POST['categoriaID'];
+      $comandoSql1 = "INSERT INTO PRODUTO(PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, CATEGORIA_ID, PRODUTO_ATIVO) VALUES (:nome, :desc, :preco, :desconto, :categoriaID)";
+      $pdo->prepare($comandoSql1)->execute(["nome" => $nome, "descricao" => $desc, "preco" => $preco, "desconto" => $categoriaID, "categoriaID" => $categoriaID,]);
+      $idProduto = $pdo->lastInsertId();
+      $comandoSql2 = "INSERT INTO PRODUTO_ESTOQUE(PRODUTO_ID, PRODUTO_QTD) VALUES(:idProduto, :qtd)";
+      $pdo->prepare($comandoSql2)->execute(["idProduto" => $idProduto, "qtd" => $quantidade]);
+    }
  }
-?>
+ 
+ header("Location: telaGerenciarprodutos.php");
