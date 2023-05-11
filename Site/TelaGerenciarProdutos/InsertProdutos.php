11175b1f59cdf8ce3b5bbd9a0b884aec1125dde6
Wende(ll)veloper <110984453+Wen-developer@users.noreply.github.com>
2023-05-10 21:47:06 -0300

    comando insert funcionando

--- Site/TelaGerenciarProdutos/InsertProdutos.php
+++ Site/TelaGerenciarProdutos/InsertProdutos.php
@@ -1,20 +1,47 @@
-<?php include "ConectaMySQLDelta.php";
-	$pdo = conectSQL();
- if (isset($_POST['enviar'])) {
-    if (!empty($_POST['nomeProduto'])|| !empty($_POST['descProduto']) || !empty($_POST['categoriaProd']) || !empty($_POST['qntProduto']) || !empty($_POST['precoProduto']) || !empty($_POST['descontoProduto']) || !empty($_POST['categoriaID'])) {
-      $nome = $_POST['nomeProduto'];
-      $preco = $_POST['precoProduto'];
-      $desc = $_POST['descProduto'];
-      $categoria = $_POST['categoriaProd'];
-      $quantidade = $_POST['qntProduto'];
-      $desconto = $_POST['descontoProduto'];
-      $categoriaID = $_POST['categoriaID'];
-      $comandoSql1 = "INSERT INTO PRODUTO(PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, CATEGORIA_ID, PRODUTO_ATIVO) VALUES (:nome, :desc, :preco, :desconto, :categoriaID)";
-      $pdo->prepare($comandoSql1)->execute(["nome" => $nome, "descricao" => $desc, "preco" => $preco, "desconto" => $categoriaID, "categoriaID" => $categoriaID,]);
-      $idProduto = $pdo->lastInsertId();
-      $comandoSql2 = "INSERT INTO PRODUTO_ESTOQUE(PRODUTO_ID, PRODUTO_QTD) VALUES(:idProduto, :qtd)";
-      $pdo->prepare($comandoSql2)->execute(["idProduto" => $idProduto, "qtd" => $quantidade]);
-    }
- }
+<?php
+  function validar() {
+    return !empty($_POST['nomeProduto'])
+    || !empty($_POST['descProduto'])
+    || !empty($_POST['qntProduto'])
+    || !empty($_POST['precoProduto'])
+    || !empty($_POST['descontoProduto'])
+    || !empty($_POST['categoriaID'])
+    || !empty($_POST['produtoAtivo']);
+  }
+
+  include "ConectaMySQLDelta.php";
+  include_once "abrir_transacao.php";
+  if (validar()) {
+    $nome = $_POST['nomeProduto'];
+    $preco = $_POST['precoProduto'];
+    $descricao = $_POST['descProduto'];     
+    $quantidade = $_POST['qntProduto'];
+    $ativo = $_POST['produtoAtivo'];
+    echo (int) $ativo;
+    $desconto = $_POST['descontoProduto'];
+    $categoriaID = $_POST['categoriaID'];
+    
+    $comandoInsertProduto = ""
+        . "INSERT INTO PRODUTO (PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, CATEGORIA_ID, PRODUTO_ATIVO) "
+        . "VALUES (:nome, :descricao, :preco, :desconto, :categoriaID, :ativo)";
+    $stmt = $pdo->prepare($comandoInsertProduto);
+    $stmt->bindValue("nome", $nome, PDO::PARAM_STR);
+    $stmt->bindValue("descricao", $descricao, PDO::PARAM_STR);
+    $stmt->bindValue("preco", (int) $preco, PDO::PARAM_INT);
+    $stmt->bindValue("desconto", (int) $desconto, PDO::PARAM_INT);
+    $stmt->bindValue("categoriaID", (int) $categoriaID, PDO::PARAM_INT);
+    $stmt->bindValue("ativo", (int) $ativo, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $idProduto = $pdo->lastInsertId();
+    $comandoInsertEstoque = "INSERT INTO PRODUTO_ESTOQUE (PRODUTO_ID, PRODUTO_QTD) VALUES (:idProduto, :qtd)";
+    $pdo->prepare($comandoInsertEstoque)->execute(["idProduto" => $idProduto, "qtd" => $quantidade]);
+
+    $transacaoOk = true;
+
+    include "fechar_transacao.php";
+  } else {
+    // Volta pra página com todos os dados.
+  }
  
  header("Location: telaGerenciarprodutos.php");
